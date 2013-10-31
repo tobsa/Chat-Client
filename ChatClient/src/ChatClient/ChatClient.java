f66a2cc9c8508536e70a029343c7bb3fad03d72c
Tobias Savinainen <tobias@savinainen.se>
2013-10-31 21:00:14 +0100

    Now creates a chat client

--- ChatClient/src/ChatClient/ChatClient.java
+++ ChatClient/src/ChatClient/ChatClient.java
@@ -1,5 +1,13 @@
 package ChatClient;
 
+import javax.swing.JFrame;
+
 public class ChatClient {
-    
+    public static void main(String[] args) {
+        ChatFrame frame = new ChatFrame();
+        frame.setTitle("ChatClient");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
