43f80415b60faf372461d4ec8b451217c49f233e
Tobias Savinainen <tobias@savinainen.se>
2013-10-31 21:05:01 +0100

    Button dispatches messages to all its listeners

--- ChatClient/src/ChatClient/MessageField.java
+++ ChatClient/src/ChatClient/MessageField.java
@@ -3,6 +3,8 @@ package ChatClient;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,6 +12,7 @@ import javax.swing.JTextField;
 public class MessageField extends JPanel {
     private JTextField textField = new JTextField(45);
     private JButton button = new JButton("Send");
+    private List<IMFListener> listeners = new ArrayList();
     
     public MessageField() {
         setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -20,10 +23,22 @@ public class MessageField extends JPanel {
         add(button);
     }
     
+    public void registerListener(IMFListener listener) {
+        listeners.add(listener);
+    }
+        
     private class ButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            String message = textField.getText();
             
+            if(message.isEmpty())
+                return;
+            
+            textField.setText("");
+            
+            for(IMFListener listener : listeners)
+                listener.dispatchMessage(message);
         }
     }
 }
